841185ebe9ea591e23b5800728acad89218c4fb2
nelsonrafael <nelsonrafaelmartins93@gmail.com>
2019-01-10 16:35:00 +0000

    Fixed Bug in client
    Made client main delete the lockfile created

--- VicaimaChatClient/src/main/Main.java
+++ VicaimaChatClient/src/main/Main.java
@@ -1,8 +1,10 @@
 package main;
 
 import java.awt.Dimension;
+import java.io.File;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 
 import javax.swing.JFrame;
 
@@ -11,12 +13,10 @@ import client.ChatClient;
 public class Main {
 	public static void main(String[] args) {
 		try {
-			@SuppressWarnings("resource")
-			RandomAccessFile randomFile = new RandomAccessFile("single.class", "rw");
-			FileChannel channel = randomFile.getChannel();
-			if (channel.tryLock() == null)
-				System.out.println("Already Running...");
-			else {
+			final File file = new File("single.class");
+			final RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+			final FileLock fileLock = randomAccessFile.getChannel().tryLock();
+			if (fileLock != null) {
 				ChatClient client = new ChatClient();
 				client.setTitle("Vicaima Chat Client");
 				client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,9 +25,20 @@ public class Main {
 				client.setLocationRelativeTo(null);
 				client.pack();
 				client.setVisible(true);
+				Runtime.getRuntime().addShutdownHook(new Thread() {
+					public void run() {
+						try {
+							fileLock.release();
+							randomAccessFile.close();
+							file.delete();
+						} catch (Exception e) {
+							System.out.println("Unable to remove lock file.\n" + e);
+						}
+					}
+				});
 			}
 		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
+			System.out.println("Unable to create and/or lock file.\n" + e);
+		}		
 	}
 }
